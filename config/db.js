b81a2873cd8eff0af9434071cdab75ebd26d8a1f
Zoranildo Santos <zoranildo22@gmail.com>
2019-06-05 13:03:00 -0300

    deleted db.js

--- config/db.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb://blogdozora:a123456@ds233167.mlab.com:33167/blogapp-zora'}
-}else{
-    module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
-}
