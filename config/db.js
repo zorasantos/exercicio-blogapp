c22afe0190d182c9db1b824c6932524e6d21599e
Zora Santos <zoranildo22@gmail.com>
2019-06-05 10:13:26 -0300

    'blogapp'

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb://blogdozora:a12345@ds233167.mlab.com:33167/blogapp-zora'}
+    module.exports = {mongoURI: 'mongodb://blogdozora:a123456@ds233167.mlab.com:33167/blogapp-zora'}
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
-}    
+}
